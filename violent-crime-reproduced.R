c67ec4a9e5180cc65ed435be7dbfba377e20c41d
ianfrantz <ian@ianfrantz.com>
2017-02-24 21:16:32 -0800

    Trimmed down code and testing visuals

--- violent-crime-reproduced.R
+++ violent-crime-reproduced.R
@@ -1,15 +1,21 @@
 #Install libraries below first
 
-library (rvest)
 library (ggplot2)
 library (knitr)
 library (XML)
+library (dplyr)
 
 #Use phantomjs to create html file so rvest can read table data
 system("./phantomjs fbi.js")
 
-crimedataurl <- ("https://ucr.fbi.gov/crime-in-the-u.s/2015/crime-in-the-u.s.-2015/tables/table-1")
-
+#Build the crimedata Dataframe from the fbi.html 
 crimedata <- readHTMLTable("fbi.html")
 crimedata <- crimedata[1] %>% as.data.frame
 
+#Use dpylr to select only two columns and rename them
+crimedata <- select(crimedata,NULL..Year, NULL..Violentcrime2)
+crimedata <- rename(crimedata, Year = NULL..Year, Violent_Crimes = NULL..Violentcrime2)
+
+ggplot(crimedata, aes(crimedata$Violent_Crime, crimedata$Year))+
+  geom_point () +
+  geom_line()
